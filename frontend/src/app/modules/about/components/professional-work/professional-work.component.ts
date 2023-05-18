5946f07788793d397e67737517ab826717027d2e
Emmanuel Toledo <emmanueltoledo1082@gmail.com>
2023-05-17 19:29:39 -0500

    Updated about components. Added new svg icons.

---
+++ frontend/src/app/modules/about/components/professional-work/professional-work.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-professional-work',
+  templateUrl: './professional-work.component.html',
+  styleUrls: ['./professional-work.component.css']
+})
+export class ProfessionalWorkComponent {
+
+}
